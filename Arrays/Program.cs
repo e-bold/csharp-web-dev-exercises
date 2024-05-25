518ad5ea11a25165127173441603250ba7cb2425
e-bold <enkhmunkh@att.net>
2024-05-25 17:38:11 -0500

    Revisited Chapter 3 and completed the Array exercise

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -2,5 +2,29 @@
 
 //TODO: Write your code for Array Practice below:
 
-Console.WriteLine("Hello, Arrays!");
+int[] numbers = {1, 1, 2, 3, 5, 8};
+
+foreach(int number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("------------");
+
+
+for (int i = 0; i < numbers.Length; i++) {
+    Console.WriteLine(numbers[i]);
+}
+
+Console.WriteLine("------------");
+
+//Prints odd ones
+foreach(int num in numbers) {
+  
+    if (num % 2 != 0 ) {
+
+    Console.WriteLine(num);
+    }
+}
+
 
