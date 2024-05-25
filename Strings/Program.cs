62b76f69918fad26abd132e7a9ef6cb5bff76d63
e-bold <enkhmunkh@att.net>
2024-05-25 18:12:31 -0500

    Revisited Chapter3 and completed Strings Practice

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -2,5 +2,13 @@
 
 //TODO: Write your code for String Practice below:
 
-Console.WriteLine("Hello, Strings!");
+string sentence = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
 
+string[] words = sentence.Split(" ");
+Console.WriteLine(words[2]);
+Console.WriteLine(string.Join(",", words));
+
+
+//Splitting by sentence
+string[] sentenceSplit = sentence.Split(".");
+Console.WriteLine(string.Join(" --- ", sentenceSplit));
