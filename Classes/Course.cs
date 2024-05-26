8b06fa04a3b67aac87a6233f2ddc334b6de8cf45
e-bold <enkhmunkh@att.net>
2024-05-26 00:02:31 -0500

    Revisited the studio and added few lines of code

---
+++ Classes/Course.cs
@@ -0,0 +1,18 @@
+﻿namespace Classes;
+
+public class Course
+{
+
+    private string Topic {get; set;}
+    private string Instructor {get; set;}
+    private List<Student> EnrolledStudents {get; set;}
+
+
+    
+    public Course(string topic, string instructor, List<Student> enrolledStudents)
+    {
+        this.Topic = topic;
+        this.Instructor = instructor;
+        this.EnrolledStudents = enrolledStudents;
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,1 +1,26 @@
 ﻿// TODO: Instantiate your objects and test your exercise solutions with print statements here.
+using Classes;
+
+Student enkhmunkh = new Student("Enkhmunkh",1,5,4.0);
+Student zachariah = new Student("Zach");
+Student peter = new Student("Peter");
+peter.NumberOfCredits = 25;
+Console.WriteLine("The Student class works! " + enkhmunkh.Name + " is a student! " + "His student id is " + enkhmunkh.StudentId);
+Console.WriteLine("The Student class works! " + zachariah.Name + " is a student! " + "His student id is " + zachariah.StudentId);
+Console.WriteLine("The Student class works! " + peter.Name + " is a student! " + "His student id is " + peter.StudentId + " and his number of credits is " + peter.NumberOfCredits);
+
+
+List<Student> students = new()
+{
+    enkhmunkh,
+    zachariah,
+    peter
+};
+
+Course subject = new Course("Geology", "Bailey", students);
+
+foreach(Student student in students)
+{
+    Console.WriteLine(student.Name);
+}
+
