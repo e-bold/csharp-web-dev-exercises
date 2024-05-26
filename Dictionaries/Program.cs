b9f881e7bde3e9a0dfc25a7cd15027830875a59d
e-bold <enkhmunkh@att.net>
2024-05-25 19:14:24 -0500

    Revisited Chapter 3 and completed Dictionary exercise

--- Dictionaries/Program.cs
+++ Dictionaries/Program.cs
@@ -4,3 +4,36 @@
 
 Console.WriteLine("Hello, Dictionary!");
 
+Dictionary <int,string> students = new Dictionary<int, string>();
+int number = 0;
+
+
+
+while(true)
+{
+    Console.Write("Please enter student Name: ");
+
+    string studentName = Console.ReadLine();
+
+    if (studentName != "")
+    {
+        Console.Write("ID: ");
+        int studentId = int.Parse(Console.ReadLine());
+        students.Add(studentId, studentName);
+        number++;
+    }
+    else
+    {
+        break;
+    }
+}
+
+Console.WriteLine("\nHere is the student list:");
+foreach(KeyValuePair<int,string> student in students)
+{
+    Console.WriteLine(student.Value + "  " + student.Key);
+}
+
+Console.WriteLine("The student list has " + number + " students.");
+
+
